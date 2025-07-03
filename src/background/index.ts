5b6d4cad997a425ef824e1f297a1cc472ac929de
Ilya Murashka <belbelialz888@gmail.com>
2025-07-03 19:24:22 +0200

    Update version to 2.0.10 in package.json and manifest files; add welcome.html and enhance permissions in manifest-dev.json

--- src/background/index.ts
+++ src/background/index.ts
@@ -304,3 +304,11 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
     }
     return true
 })
+
+chrome.runtime.onInstalled.addListener((details) => {
+    if (details.reason === 'install') {
+        chrome.tabs.create({
+            url: chrome.runtime.getURL('welcome.html'),
+        })
+    }
+})
